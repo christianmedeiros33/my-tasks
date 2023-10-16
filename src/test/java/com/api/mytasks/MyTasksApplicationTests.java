d91ad260ab2a8de06a650c1175fc540be858244e
christian medeiros <christianmedeiros022@gmail.com>
2023-10-16 20:46:16 -0300

    project structure.

---
+++ src/test/java/com/api/mytasks/MyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mytasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
